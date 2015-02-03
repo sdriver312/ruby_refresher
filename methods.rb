22475df488fcad27c8c6dc0951cc4e7170f271db
sdriver312 <gsteers@gmail.com>
2015-02-02 20:07:34 -0500

    Some new files

---
+++ methods.rb
@@ -0,0 +1,25 @@
+def welcome
+	puts "Hello World!"
+end
+
+def add
+	puts 1 + 1
+end
+
+def longest_word
+	words = ['apple', 'pear', 'banana', 'plum']
+	longest_word = words.inject do |memo, word| 
+		memo.length > word.length ? memo : word
+	end
+	puts longest_word
+end
+
+def over_five?
+	value = 3
+	puts value > 5 ? 'Over 5' : 'Not over 5'
+end
+
+welcome
+longest_word
+over_five?
+add
